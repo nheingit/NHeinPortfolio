a2786c5d6e75449904b72d84fbcae4a8b6d02201
Noah <nheindev@gmail.com>
2021-01-20 17:34:38 -0600

    added styling for mobile on new page

--- src/views/HelpingPointsPage.svelte
+++ src/views/HelpingPointsPage.svelte
@@ -1,35 +1,35 @@
-<div class='h-screen bg-gray-100'>
-  <div class='container'>
-    <div>
-      <h2>What I can do to help</h2>
-      <p>You have a business, and I provide web applications, websites, or mobile applications for <span>your</span> needs</p>
+<div class='h-auto bg-gray-100'>
+  <div class='container p-6 text-center'>
+    <div class='mx-4 mb-4'>
+    <h2 class='font-serif text-2xl font-bold'>What I can do to help</h2>
+      <p class='text-xl'>You have a business, and I provide web applications, websites, and mobile applications for <span>your</span> needs</p>
     </div>
-    <div>
-      <h3>Wondering how it's done?</h3>
-      <p>It's as easy as...</p>
-    <div class='bg-true-blue'>
-      <div>
-        <div>
-        <img src="lightBulbIcon.svg" alt="icon of a lightbulb">
+    <div class='my-8'>
+      <h3 class='font-serif text-xl font-bold'>Wondering how it's done?</h3>
+      <p class='text-xl'>It's as easy as...</p>
+    <div class='py-5 rounded-full bg-true-blue'>
+      <div class='m-8'>
+        <div class='m-2'>
+        <img class='mx-auto' src="lightBulbIcon.svg" alt="icon of a lightbulb">
         <p>Design</p>
         </div>
         <p>Lorem Ipsum</p>
       </div>
-      <div>
+      <div class='m-8'>
         <div>
-          <img src="paperIcon.svg" alt="icon of a pen and paper">
+          <img class='mx-auto' src="paperIcon.svg" alt="icon of a pen and paper">
           <p>Develop</p>
         </div>
         <p>Lorem Ipsum</p>
       </div>
       <div>
         <div>
-          <img src="checkmarkIcon.svg" alt="icon of a checkmark">
+          <img class='mx-auto' src="checkmarkIcon.svg" alt="icon of a checkmark">
           <p>Done</p>
         </div>
         <p>Lorem Ipsum</p>
       </div>
-      <button type='button'>Book a Call</button>
+      <button class='p-2 px-8 my-3 text-xl text-white bg-yellow-400 rounded-2xl focus:outline-none focus:ring-4 hover:bg-yellow-500' type='button'>Book a Call</button>
     </div>
 
     </div>
