141f7b7fc7179165856044dafded6252e78a5d91
Noah <nheindev@gmail.com>
2021-01-21 17:53:39 -0600

    added html for new page

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
 	import HomePage from './views/HomePage.svelte';
 	import SellingPointsPage from './views/SellingPointsPage.svelte';
 	import HelpingPointsPage from './views/HelpingPointsPage.svelte';
+	import ValuePointsPage from './views/ValuePointsPage.svelte';
 </script>
 
 <Tailwindcss />
 <HomePage/>
 <SellingPointsPage/>
 <HelpingPointsPage/>
+<ValuePointsPage/>
---
+++ src/views/ValuePointsPage.svelte
@@ -0,0 +1,14 @@
+<div class='h-auto bg-true-blue'>
+  <div class="container">
+    <img src="downArrow.svg" alt="arrow pointing from avatar down to button">
+    <img src='gardenMe.png' alt='Noah with garden in background'>
+    <div class="bg-gray-100">
+      <p> Hi I'm Noah!</p>
+      <h1>I help solo entrepreneurs and startups grow their online business</h1>
+      <p>You have a proudct or service that delivers RESULTS to your clients</p>
+      <p>Building this business is your mission, and you're ready to take it online</p>
+      <p>I create website that go from landing pages, and -commerce, all the way to full service web applications that can even be published on the appstore!</p>
+    </div>
+    <button class='bg-yellow-400'>More About Me</button>
+  </div>
+</div>
