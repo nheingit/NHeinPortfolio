9806945c87eb6b20de7eaf0a017f389f2b9430b3
Noah <nheindev@gmail.com>
2021-01-20 22:48:41 -0600

    finished 3rd page on the home route

--- src/views/HelpingPointsPage.svelte
+++ src/views/HelpingPointsPage.svelte
@@ -1,37 +1,37 @@
 <div class='h-auto bg-gray-100'>
   <div class='container p-6 text-center'>
     <div class='mx-4 mb-4'>
-    <h2 class='font-serif text-2xl font-bold'>What I can do to help</h2>
-      <p class='text-xl'>You have a business, and I provide web applications, websites, and mobile applications for <span>your</span> needs</p>
+    <h2 class='font-serif text-2xl font-bold sm:text-4xl lg:text-6xl lg:my-2 2xl:text-7xl'>What I can do to help</h2>
+      <p class='text-xl sm:text-2xl lg:text-3xl 2xl:text-4xl'>You have a business, and I provide web applications, websites, and mobile applications for <span>your</span> needs</p>
     </div>
     <div class='my-8'>
-      <h3 class='font-serif text-xl font-bold'>Wondering how it's done?</h3>
-      <p class='text-xl'>It's as easy as...</p>
-    <div class='py-5 rounded-full bg-true-blue'>
+      <h3 class='font-serif text-xl font-bold sm:text-2xl lg:text-3xl 2xl:text-5xl'>Wondering how it's done?</h3>
+      <p class='text-xl sm:text-2xl lg:text-3xl 2xl:text-5xl'>It's as easy as...</p>
+    <div class='py-5 rounded-full bg-true-blue sm:m-12 md:flex md:px-10 lg:py-10'>
       <div class='m-8'>
-        <div class='m-2'>
-        <img class='mx-auto' src="lightBulbIcon.svg" alt="icon of a lightbulb">
-        <p>Design</p>
+        <div>
+        <img class='mx-auto 2xl:h-36' src="lightBulbIcon.svg" alt="icon of a lightbulb">
+        <p class='text-xl font-bold lg:text-2xl 2xl:text-4xl'>Design</p>
         </div>
-        <p>Lorem Ipsum</p>
+        <p class='lg:text-xl lg:pt-2 2xl:text-3xl'>We collaborate to find a design that represents your brand</p>
       </div>
       <div class='m-8'>
         <div>
-          <img class='mx-auto' src="paperIcon.svg" alt="icon of a pen and paper">
-          <p>Develop</p>
+          <img class='mx-auto 2xl:h-36' src="paperIcon.svg" alt="icon of a pen and paper">
+          <p class='text-xl font-bold 2xl:text-4xl lg:text-2xl'>Develop</p>
         </div>
-        <p>Lorem Ipsum</p>
+        <p class='lg:text-xl lg:pt-2 2xl:text-3xl'>I give you weekly updates on how our project is coming along</p>
       </div>
-      <div>
+      <div class='m-8'>
         <div>
-          <img class='mx-auto' src="checkmarkIcon.svg" alt="icon of a checkmark">
-          <p>Done</p>
+          <img class='mx-auto 2xl:h-36' src="checkmarkIcon.svg" alt="icon of a checkmark">
+          <p class='text-xl font-bold 2xl:text-4xl lg:text-2xl'>Done</p>
         </div>
-        <p>Lorem Ipsum</p>
+        <p class='lg:text-xl lg:pt-2 2xl:text-3xl'>Once you give it the seal of approval, I put it online!</p>
       </div>
-      <button class='p-2 px-8 my-3 text-xl text-white bg-yellow-400 rounded-2xl focus:outline-none focus:ring-4 hover:bg-yellow-500' type='button'>Book a Call</button>
+      <button class='p-2 px-8 my-3 text-xl text-white bg-yellow-400 md:hidden rounded-2xl focus:outline-none focus:ring-4 hover:bg-yellow-500' type='button'>Book a Call</button>
     </div>
-
+      <button class='hidden px-32 py-6 my-3 text-xl text-white bg-yellow-400 2xl:px-40 2xl:text-4xl md:block md:mx-auto rounded-2xl focus:outline-none focus:ring-4 hover:bg-yellow-500' type='button'>Book a Call</button>
     </div>
   </div>
 </div>
