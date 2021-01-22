3208b63bf6458ee4282c13d3d901d0cce2cf42fd
Noah <nheindev@gmail.com>
2021-01-21 22:38:37 -0600

    added more pages and am working on making them more responsive

---
+++ src/views/SocialProofPage.svelte
@@ -0,0 +1,10 @@
+<div class="bg-gray-100">
+  <div class="container">
+    <div class="rounded-full bg-true-blue">
+      <img alt='pic of joseph'>
+      <div class="bg-gray-100">
+        <p>Lorem Ipsum this is a review of joseph saying some nice things about me, link to <a href='jwcuisine.io'>My New Website!</a></p>
+      </div>
+    </div>
+  </div>
+</div>
--- src/views/ValuePointsPage.svelte
+++ src/views/ValuePointsPage.svelte
@@ -1,14 +1,14 @@
 <div class='h-auto bg-true-blue'>
-  <div class="container">
-    <img src="downArrow.svg" alt="arrow pointing from avatar down to button">
-    <img src='gardenMe.png' alt='Noah with garden in background'>
-    <div class="bg-gray-100">
-      <p> Hi I'm Noah!</p>
-      <h1>I help solo entrepreneurs and startups grow their online business</h1>
-      <p>You have a proudct or service that delivers RESULTS to your clients</p>
-      <p>Building this business is your mission, and you're ready to take it online</p>
-      <p>I create website that go from landing pages, and -commerce, all the way to full service web applications that can even be published on the appstore!</p>
+  <div class="container relative py-5">
+    <img class='absolute left-0 bottom-14 h-4/5'src="downArrow.svg" alt="arrow pointing from avatar down to button">
+    <img class='relative z-10 w-56 ml-20' src='gardenMe.png' alt='Noah with garden in background'>
+    <div class="ml-20 mr-4 bg-gray-100 rounded-3xl">
+      <p class='p-2 px-4 tracking-wider'> Hi I'm Noah!</p>
+      <h1 class='p-2 px-4 font-serif text-xl font-bold'>I help solo entrepreneurs and startups grow their online business</h1>
+      <p class='px-4 py-2'>You have a proudct or service that delivers RESULTS to your clients</p>
+      <p class='px-4 py-2'>Building this business is your mission, and you're ready to take it online</p>
+      <p class='px-4 py-2'>I create websites that go from landing pages, and e-commerce, all the way to full service web applications that can even be published on the appstore!</p>
     </div>
-    <button class='bg-yellow-400'>More About Me</button>
+    <button class='p-2 px-12 mt-16 mb-2 ml-20 text-lg text-white bg-yellow-400 focus:outline-none focus:ring-4 focus:ring-true-blue hover:bg-yellow-500'>More About Me</button>
   </div>
 </div>
