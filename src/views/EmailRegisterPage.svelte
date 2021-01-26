827d39018e451a2490f5895c934509caae76a8c8
Noah <nheindev@gmail.com>
2021-01-26 12:40:54 -0600

    working on changing the layout on larger screens for the email page

--- src/views/EmailRegisterPage.svelte
+++ src/views/EmailRegisterPage.svelte
@@ -2,22 +2,42 @@
   p{
     @apply py-1;
     @apply text-lg;
+    @apply text-center;
+    @apply lg:text-xl;
+  }
+  input[type=email]{
+    @apply w-full;
+    @apply p-2;
+    @apply bg-gray-200;
+    @apply m-2;
+    @apply border-0;
+    @apply border-b-2;
+    @apply border-yellow-400;
+  }
+  input[type=text]{
+    @apply w-full;
+    @apply bg-gray-200;
+    @apply p-2;
+    @apply m-2;
+    @apply border-0;
+    @apply border-b-2;
+    @apply border-yellow-400;
   }
 </style>
 <div class='w-auto h-auto p-6 bg-true-blue'>
-    <div class="container p-3 mx-auto bg-gray-100 rounded-3xl">
-      <p class='tracking-widest text-gray-500'>Free download</p>
-      <h4 class='text-4xl font-bold'>Have a free homepage!</h4>
-      <img src="homePage.png" alt="slanted smaller version of the homepage seen above">
+    <div class="container p-10 mx-auto bg-gray-100 rounded-full">
+      <p class='tracking-widest text-gray-500 sm:text-center'>Free download</p>
+      <h4 class='text-4xl font-bold text-center'>Have a free homepage!</h4>
+      <img class='m-3 sm:mx-auto sm:w-3/5 lg:w-1/3' src="homePage.png" alt="slanted smaller version of the homepage seen above">
       <p> You can see the principles I used to design my homepage so you can apply it to your own website.</p>
       <p>See if you like what I provide.</p>
       <p>All For free!</p>
-      <form>
+      <form class='md:px-16 lg:mx-12'>
         <input type='text' name='fName' placeholder='First Name'>
         <input type='email' name='email' placeholder='email'>
-        <input type='submit' value='Get It Now'>
+        <input  class='w-full p-2 m-2 bg-gray-200 border-2 border-yellow-400' type='submit' value='Get It Now'>
       </form>
-      <p>This adds you to my email list. I send out free content that I think you will find valuable or interesting
+      <p class='mx-8 mb-8 text-center sm:mx-12 md:mx-16 lg:px-16'>This adds you to an emailing list. you will get curated content relevant to you delivered to your inbox
         You can unsubscribe at any time.
       </p>
     </div>
--- src/views/SellingPointsPage.svelte
+++ src/views/SellingPointsPage.svelte
@@ -1,6 +1,6 @@
 
-<div class='h-screen bg-cover bg-office-background sm:bg-true-blue sm:bg-none'>
-  <div class='container max-w-full max-h-screen sm:pt-0 lg:relative'>
+<div class='w-auto h-auto py-5 bg-cover bg-office-background sm:bg-true-blue sm:bg-none'>
+  <div class='container w-auto h-auto sm:pt-0 lg:relative'>
     <img class='relative z-20 hidden object-cover w-4/5 mx-auto sm:block sm:h-72 sm:w-full rounded-3xlmd:w-3/5 lg:w-1/2 lg:my-48 lg:pl-5 lg:rounded-3xl lg:h-2/3 lg:absolute xl:h-screen xl:m-0 xl:p-0 xl:w-1/3 2xl:w-2/5' src='startupOffice.png' alt='workers talking while sitting in an open-office'>
     <!--<div class='absolute hidden w-2/3 mx-auto bg-gray-100 rounded-3xl sm:h-96 md:w-1/5 lg:block'></div>-->
     <div class='relative w-3/4 mx-auto my-auto bg-gray-100 top-2 rounded-3xl sm:h-auto md:w-4/5 lg:mr-8 lg:pl-36 lg:w-3/5 lg:h-screen-4/5 lg:top-24 lg:py-12 xl:px-3 xl:py-12 xl:my-auto 2xl:mr-4'>
