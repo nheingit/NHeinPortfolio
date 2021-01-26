eb3e889012ad50708852344513603e42f03e3258
Noah <nheindev@gmail.com>
2021-01-25 23:26:36 -0600

    added email registration page optimized only for mobile currently

---
+++ src/views/EmailRegisterPage.svelte
@@ -0,0 +1,24 @@
+<style>
+  p{
+    @apply py-1;
+    @apply text-lg;
+  }
+</style>
+<div class='w-auto h-auto p-6 bg-true-blue'>
+    <div class="container p-3 mx-auto bg-gray-100 rounded-3xl">
+      <p class='tracking-widest text-gray-500'>Free download</p>
+      <h4 class='text-4xl font-bold'>Have a free homepage!</h4>
+      <img src="homePage.png" alt="slanted smaller version of the homepage seen above">
+      <p> You can see the principles I used to design my homepage so you can apply it to your own website.</p>
+      <p>See if you like what I provide.</p>
+      <p>All For free!</p>
+      <form>
+        <input type='text' name='fName' placeholder='First Name'>
+        <input type='email' name='email' placeholder='email'>
+        <input type='submit' value='Get It Now'>
+      </form>
+      <p>This adds you to my email list. I send out free content that I think you will find valuable or interesting
+        You can unsubscribe at any time.
+      </p>
+    </div>
+</div>
