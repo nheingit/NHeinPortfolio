fbc0ad8f559a5475c9721afe73064c1d182c5b1d
Noah <nheindev@gmail.com>
2021-02-02 15:00:30 -0600

    changed href from /about to about

--- src/homeViews/ValuePointsPage.svelte
+++ src/homeViews/ValuePointsPage.svelte
@@ -1,7 +1,7 @@
 <div class='h-auto bg-true-blue'>
   <div class="container relative py-5 mx-auto">
-    <img class='absolute left-0 bottom-14 h-4/5'src="downArrow.svg" alt="arrow pointing from avatar down to button">
-    <img class='relative w-56 ml-20 lg:w-80' src='gardenMe.png' alt='Noah with garden in background'>
+    <img loading='lazy' class='absolute left-0 bottom-14 h-4/5'src="downArrow.svg" alt="arrow pointing from avatar down to button">
+    <img loading='lazy' class='relative w-56 ml-20 lg:w-80' src='gardenMe.png' alt='Noah with garden in background'>
     <div class="ml-20 mr-4 bg-gray-100 rounded-3xl">
       <p class='p-2 px-4 tracking-wider'> Hi I'm Noah!</p>
       <h1 class='p-2 px-4 font-serif text-xl font-bold lg:text-3xl'>I help solo entrepreneurs and startups grow their online business</h1>
@@ -9,6 +9,6 @@
       <p class='px-4 py-2'>Building this business is your mission, and you're ready to take it online</p>
       <p class='px-4 py-2'>I create websites that go from landing pages, and e-commerce, all the way to full service web applications that can even be published on the appstore!</p>
     </div>
-    <button onclick="location.href='/about'" class='p-2 px-12 mt-16 mb-2 ml-20 text-lg text-white bg-yellow-400 focus:outline-none focus:ring-4 focus:ring-true-blue hover:bg-yellow-500'>More About Me</button>
+    <button onclick="location.href='about'" class='p-2 px-12 mt-16 mb-2 ml-20 text-lg text-white bg-yellow-400 focus:outline-none focus:ring-4 focus:ring-true-blue hover:bg-yellow-500'>More About Me</button>
   </div>
 </div>
